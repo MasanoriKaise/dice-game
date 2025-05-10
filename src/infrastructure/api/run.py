3e6fa54c2c6ab305a738987fbe3d6cec5442f962
Masanori Kaise <210454188+MasanoriKaise@users.noreply.github.com>
2025-05-10 10:01:16 +0900

    Refactor: Move config and run files to appropriate locations in infrastructure (#8)

--- src/infrastructure/api/run.py
+++ src/infrastructure/api/run.py
@@ -1,7 +1,7 @@
 """アプリケーション起動スクリプト"""
 import uvicorn
 from src.infrastructure.api.main import app
-from config.api import API_HOST, API_PORT
+from src.infrastructure.api.config import API_HOST, API_PORT
 
 if __name__ == "__main__":
     uvicorn.run(
